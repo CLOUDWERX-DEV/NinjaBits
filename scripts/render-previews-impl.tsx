214dc7a83a67edb9540c57898e4eb3a621c7e854
cloudwerx.dev <sysop@cloudwerx.dev>
2026-07-12 15:16:34 -0400

    style: change SVG preview window title to NinjaBits

--- scripts/render-previews-impl.tsx
+++ scripts/render-previews-impl.tsx
@@ -79,7 +79,7 @@ function makeStore(
 ): Store {
   const noop = (): void => {};
   return {
-    config: { downloadDir: "~/Downloads/torlink" } as Config,
+    config: { downloadDir: "~/Downloads/NinjaBits" } as Config,
     setConfig: noop,
     queue: fakeQueue(items, history, seeds),
     view: "browser",
@@ -129,7 +129,7 @@ function save(
   }
   writeFileSync(
     join(OUT_DIR, `${name}.svg`),
-    ansiToSvg(frame, { cols: COLS, title: "torlink", ...extra }),
+    ansiToSvg(frame, { cols: COLS, title: "NinjaBits", ...extra }),
   );
   console.log(`preview/${name}.svg`);
 }
